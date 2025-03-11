a8740dc8fe3d2bc0c804e6637413c65fdbf1181a
JaumVictorr <167575735+JaumVictorr@users.noreply.github.com>
2025-03-11 07:57:26 -0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+import os
 SECRETY_KEY = 'CHAVE_SECRETA_SECRETISSIMA'
 DEBUG = True
 DB_HOST = 'localhost'
-DB_NAME = r'C:\Users\Aluno\Desktop\BANCO.FDB'
+DB_NAME = r'C:\Users\Aluno\Downloads\Biblioteca-205\BANCO.FDB'
 DB_USER = 'sysdba'
 DB_PASSWORD = 'sysdba'
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
